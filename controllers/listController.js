98e23db4ab3fdb9c9ce9363babcb6edb8f087b84
HectorGuerrini <hectorumaga@hotmail.com>
2018-06-11 23:54:28 -0300

    init 2

--- controllers/listController.js
+++ controllers/listController.js
@@ -2,13 +2,13 @@ var config = require('../config')
 var sql = require('mssql')
 
 
-sql.connect(config,function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected ");
-});
+// sql.connect(config,function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected ");
+// });
 
 
 exports.home = function (req, res) {
