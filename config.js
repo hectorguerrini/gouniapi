6789fbbd9d54876fdb6054fd0ea1ad11ed9dbcbe
HectorGuerrini <hectorumaga@hotmail.com>
2018-10-03 12:37:03 -0300

    config

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
-    user: 'node',
-    password: 'nodeadmin',
-    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+    user: 'gouniadmin',
+    password: 'Luka1523',
+    server: 'gouni.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
     database: 'DBGouni'
+    
 }
 
 module.exports = config;
